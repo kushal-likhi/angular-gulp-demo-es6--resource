cf7cf8478b9fcde4be548bd799d3b6128e4ce0ca
Kushal Likhi <kushal@innox.io>
2015-09-14 17:40:45 +0530

    Added todoItem resource in module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,14 +2,14 @@
 import config from './index.config';
 import routerConfig from './index.route';
 import runBlock from './index.run';
-
+//controllers
 import MainController from './main/main.controller';
 import TodoController from './todo/todo.controller.js';
-
-import WebDevTecService from './components/webDevTec/webDevTec.service.js';
-
+//Resources
+import TodoItemResource from './common/resources/TodoItemResource.js';
+//Directives
 import NavbarDirective from './components/navbar/navbar.directive.js';
-
+//Filters
 import HumanTimeFilter from './todo/filters/filter.humanTime.js';
 
 angular.module('angularDemo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
@@ -18,8 +18,8 @@ angular.module('angularDemo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize'
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('webDevTec', WebDevTecService)
   .filter('humanTime', HumanTimeFilter)
+  .factory('TodoItem', TodoItemResource)
   .controller('MainController', MainController)
   .controller('TodoController', TodoController)
   .directive('acmeNavbar', () => new NavbarDirective());
