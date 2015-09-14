867d7ba269d49b9887ebde25760420a7472d9618
Kushal Likhi <kushal@innox.io>
2015-09-14 14:14:00 +0530

    Added listing and saving of items in todo modue

--- src/app/todo/todo.controller.js
+++ src/app/todo/todo.controller.js
@@ -1,8 +1,29 @@
 class TodoController {
-  constructor () {
+  constructor ($scope) {
     'ngInject';
 
+    this.items = [];
+    this.loading = true;
+    var self = this;
+    setTimeout(function(){
+      self.loading = false;
+      console.log(self);
+      $scope.$apply();
+    },2000);
 
+  }
+
+  getItems(){
+    return this.items || [];
+  }
+
+  saveItem(){
+    this.items.push({
+      postedAt: new Date(),
+      description: this.newItemDescription,
+      state: 'pending'
+    });
+    this.newItemDescription = null;
   }
 }
 
